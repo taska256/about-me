c31d89721f1a634df877b241e5fb91fd7ea6799c
aoiacai <102672343+aoiacai@users.noreply.github.com>
2025-04-29 18:44:55 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Background from "../components/Background.svelte";
+    import "../app.css";
+</script>
+
+<p class="fixed text-2xl text-gray-600 font-bold p-2">threejs background</p>
+<Background />
